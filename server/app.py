52b4934146dae7eb3ba0060982a0e310bc089608
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2024-01-12 15:08:47 +0300

    Completed lab

--- server/app.py
+++ server/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, make_response
 from flask_migrate import Migrate
 
-from models import db
+from models import db, Pet, Owner
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -13,6 +13,50 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+@app.route('/')
+def index():
+    response = make_response(
+        '<h1>Welcome to the pet/owner directory!</h1>',
+        200
+    )
+    return response
 
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    if not pet:
+        response_body = f'''
+            <h1>No pet found with id {id}</h1>
+        '''
+        response = make_response(response_body, 404)
+        return response
+
+    response_body = f'''
+        <h1>Information for {pet.name}</h1>
+        <h2>pet Species: {pet.species}</h2>
+        <h2>Pet Owner: {pet.owner.name}</h2>
+'''
+
+@app.route('/owner/<int:id>')
+def owner_by_id(id):
+    owner = Owner.query.filter(Owner.id == id).first()
+
+    if not owner:
+        response_body = f'''
+            <h1>No owner found with id {id}</h1>
+        '''
+        response = make_response(response_body, 404)
+        return response
+    response_body = f'<h1>Information for {owner.name}</h1>'
+    pets = [pet for pet in owner.pets]
+
+    if not pets:
+        response_body += f'<h2>Has no pets found for at this time.{owner.name}</h2>'
+    else:
+        for pet in pets:
+            response_body += f'<h2>Has pet {pet.species} named {pet.name}</h2>'    
+    response = make_response(response_body, 200)
+    return response
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
